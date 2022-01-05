eef28829251312436021b97d14727ebb81ede19c
Safwatzzz <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-01-05 14:16:55 +0200

    Base Auth using spring security with public access html page accessed through thr root -> localhost:8080
    - Configure authentication through AuthenticationManagerBuilder
    - Apply PasswordEncoder using PasswordEncoderFactories instead of using @Bean that uses bcrypt as a default encoder
    - UserName : ahmed
    - password : 12345

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.example.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,4 +23,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated().and().httpBasic();
     }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("ahmed").password(encoder.encode("12345")).roles("Student");
+    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        UserDetails user = User.builder()
+//                .username("Ahmed")
+//                .password(encoder.encode("12345"))
+//                .roles("student")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
 }
