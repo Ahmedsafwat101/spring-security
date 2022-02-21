29f48006566fe08f68c989ea7cbfa1088a364cfa
Safwatzzz <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-02-21 21:09:23 +0200

    Form Base Auth using spring security
    - Custom login.html page

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.springsecurity.utils.ApplicationUserPermissions.COURSE_WRITE;
 import static com.example.springsecurity.utils.ApplicationUserRole.*;
 
 @Configuration
@@ -30,12 +29,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
-                .authenticated().and().httpBasic();
+                .authenticated().and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
     }
 
     @Override
@@ -46,21 +44,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("anna")
                 .password(encoder.encode("anna12345"))
-               // .roles(STUDENT.name())
                 .authorities(STUDENT.getSimpleGrantedAuthorities())
                 .build();
 
         UserDetails peteParker = User.builder()
                 .username("peter")
                 .password(encoder.encode("peter12345"))
-                //.roles(ADMIN.name())
                 .authorities(ADMIN.getSimpleGrantedAuthorities())
                 .build();
 
         UserDetails tomHolland = User.builder()
                 .username("tom")
                 .password(encoder.encode("tom12345"))
-                //.roles(ADMINTRAINEE.name())
                 .authorities(ADMINTRAINEE.getSimpleGrantedAuthorities())
                 .build();
 
